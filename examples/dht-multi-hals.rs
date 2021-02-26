0108c241ea413306d5f18a7600579c6ace19e3e9
Paul Gilbert <pdgilbert@users.noreply.github.com>
2021-02-26 15:43:36 -0500

    -merged fix with overflow. -switched stm32f3xx code to use AsmDelay rather than the hals' delay::Delay which was causing crash. -run tested and working with both DHT11 and DHT22 on bluepill (stm32f103),   blackpill-stm32f401, blackpill-stm32f411, discovery-stm32f303, discovery-stm32l100.   and heltec-lora-node151 (stm32l151).

--- examples/dht-multi-hals.rs
+++ examples/dht-multi-hals.rs
@@ -6,10 +6,8 @@
 //!  These make the application code common.
 
 //!  This examples has been tested to run with both DHT11 and DHT22 on bluepill (stm32f103),
-//!  blackpill-stm32f401, and  blackpill-stm32f411. As of Feb. 22 2021 there are known
-//!  runtime errors on
-//!  discovery-stm32f303 reads both DHT11 and DHT22  once but then fails at delay.
-//!  discovery-stm32l100 gives Error Timeout with both DHT11 and DHT22.
+//!  blackpill-stm32f401, blackpill-stm32f411, discovery-stm32f303, discovery-stm32l100. 
+//!  and heltec-lora-node151 (stm32l151).
 //!
 //!  In linux with environment variables as below the example can be compiled, loaded and
 //!  run using gdb and openocd
@@ -24,9 +22,8 @@
 //!  or
 //!  	cargo  build --target $TARGET --features $HAL,$MCU,dht22 --example dht-multi-hals  [ --release ]
 //!
-//!  If --release is omitted then some MCUs do not have  sufficient memory an loading results in
+//!  If --release is omitted then some MCUs do not have  sufficient memory and loading results in
 //!       '.rodata will not fit in region FLASH '
-//!
 //!  However, even with sufficient memory the code without --release is slower and often results in an
 //!  'Error Timeout' so it is recommended to use --release when attempting to run the code.
 //!
@@ -169,7 +166,11 @@ use stm32f3xx_hal::{
 };
 
 #[cfg(feature = "stm32f3xx")]
-fn setup() -> (PA8<Output<OpenDrain>>, Delay) {
+use asm_delay::{ AsmDelay, bitrate, };
+
+#[cfg(feature = "stm32f3xx")]
+fn setup() -> (PA8<Output<OpenDrain>>, asm_delay::AsmDelay) {
+//fn setup() -> (PA8<Output<OpenDrain>>, Delay) {
     let cp = CorePeripherals::take().unwrap();
     let p = Peripherals::take().unwrap();
 
@@ -184,7 +185,9 @@ fn setup() -> (PA8<Output<OpenDrain>>, Delay) {
     pa8.set_high().ok();
 
     // delay is used by `dht-sensor` to wait for signals
-    let mut delay = Delay::new(cp.SYST, clocks); //SysTick: System Timer
+    // there is a crash that seems to be caused by this delay
+    // let mut delay = Delay::new(cp.SYST, clocks); //SysTick: System Timer
+    let mut delay = AsmDelay::new(bitrate::U32BitrateExt::mhz(16)); 
 
     //  1 second delay (for DHT setup?) Wait on  sensor initialization?
     delay.delay_ms(1000_u16);
